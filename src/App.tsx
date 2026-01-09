0ad65633865fb6564e2db70641ea8c6c153173c4
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-01-09 15:53:40 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Hero from "./components/Hero";
+import Hero from './components/Hero';
+import Portfolio from './components/Portfolio';
 
 export default function App() {
   return (
     <html lang="en" className="scroll-smooth">
       <body className="bg-zinc-900">
         <Hero />
+        <Portfolio />
       </body>
     </html>
   );
 }
-
